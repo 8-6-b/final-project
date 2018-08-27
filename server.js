f2a7e4bebf68827221a4ee098b773dfde7777f3e
Your Name <you@example.com>
2018-08-27 16:09:03 -0400

    pulled new files from remote

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json())
 
 // !!! DEVELOPMENT ONLY (start) !!! //
 
-// var corsOptions = {
-//     origin: 'http://localhost:4200',
-//     optionsSuccessStatus: 200 
-// }
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 
+}
   
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 // !!! DEVELOPMENT ONLY (end) !!! //
 
